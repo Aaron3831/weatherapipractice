188db8b72deb917289dba1e115c123a77c0162ac
Aaron Nguyen and Jason Lazzuri <aaron@>
2017-05-22 16:24:48 -0700

    setup for api

---
+++ build/js/app.js
@@ -0,0 +1,17 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+
+var apiKey = "92ab1d4d1eff495bfe2d06f2969029b7";
+
+$(document).ready(function() {
+  $('#weather-location').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.weather[0].description + "%");
+      $('.showWeather1').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    });
+  });
+});
+
+},{}]},{},[1]);
---
+++ js/weather-interface.js
@@ -0,0 +1,14 @@
+var apiKey = "92ab1d4d1eff495bfe2d06f2969029b7";
+
+$(document).ready(function() {
+  $('#weather-location').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+      .then(function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.weather[0].description);
+      $('.showWeather1').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    });
+  });
+});
